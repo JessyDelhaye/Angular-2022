a59f8fc6d622bff5b718164e7ad4b58aed17f80a
JessyDelhaye <jessy.delhaye@outlook.com>
2022-06-20 17:28:04 +0200

    Section 5 DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemon-list';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  title = 'JED app';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit(){
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[4]);
+  }
+
+  selectPokemon(pokemon: Pokemon){
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  }
 }
