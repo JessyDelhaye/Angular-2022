3cb6546e0dedbdc6b76ade62e858160671f0fd57
JessyDelhaye <jessy.delhaye@outlook.com>
2022-06-21 17:48:00 +0200

    Section 7 DONE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BorderCardDirective } from './border-card.directive';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, BorderCardDirective],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
