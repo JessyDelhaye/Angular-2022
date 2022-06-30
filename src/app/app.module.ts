56c2ddd459380a8358d9b0a02f141510d0e3243d
JessyDelhaye <jessy.delhaye@outlook.com>
2022-06-30 23:17:28 +0200

    Section 8 DONE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, BorderCardDirective],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe
+  ],
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule {}
